e8012e9955dfcdf1210b4e230699009693306dff
SongSeungGyu <sorim831@naver.com>
2024-12-20 12:51:51 +0900

    프로젝트 초안

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+const app = express();
+app.use(cors());
+app.set("port", process.env.PORT || 8000);
+
+module.exports = app;
